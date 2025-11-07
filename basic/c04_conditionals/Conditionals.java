b8c787515f5cb209e3348cfd937b73301a2e8ec3
jmartd18 <jmartd18@estudiantes.unileon.es>
2025-11-07 09:04:09 +0100

    mitad conditional hecho

--- basic/c04_conditionals/Conditionals.java
+++ basic/c04_conditionals/Conditionals.java
@@ -24,9 +24,10 @@ public class Conditionals {
 
         if (age > 18) {
             System.out.println("El usuario es mayor de edad");
-        } else if (age == 18) {
+        } else if (age == 18) {// hay que asegurarnos siempre que el if-else esta entre el if y el else. 
+            //PRIORIDAD:
             System.out.println("El usuario acaba de cumplir 18");
-        } else {
+        } else {// si pasa todo lo contrario al if, se ejecuta el else
             System.out.println("El usuario es menor de edad");
         }
 
@@ -35,8 +36,10 @@ public class Conditionals {
         Vídeo: https://youtu.be/JOAqpdM36wI?t=10678
         */
 
-        // switch
-
+        // switch: TENEMOS QUE ESTABLECER LA CONDICION. SIRVE PARA COMPROBAR EL VALOR DE UNA VARIABLE
+        //EN EL CASE ESTABLECEMOS QUE VALOR QUEREMOS SI SE CUMPLE LA CONDICION DEL SWITCH
+        //EL BREAK ROMPE CON EL CONTEXTO DE LAS LLAVES PARA SALTAR AL SIGUIENTE CASE
+        //DEFAULT: no hace falta hacer break porque es la ultima instruccion 
         var day = 5;
 
         switch (day) {
--- basic/c04_conditionals/ConditionalsExercises.java
+++ basic/c04_conditionals/ConditionalsExercises.java
@@ -10,17 +10,65 @@ public class ConditionalsExercises {
     public static void main(String[] args) {
 
         // 1. Establece la edad de un usuario y muestra si puede votar (mayor o igual a 18).
-
+        int edad = 20;
+        if( edad>18) {
+            System.out.println("Es apto para votar");
+        } else {
+            System.out.println("no es apto para votar");
+        }
         // 2. Declara dos números y muestra cuál es mayor, o si son iguales.
-
+        int numeroUno = 10;
+        int numeroDos = 9;
+        if (numeroUno < numeroDos) {
+            System.out.println("primero menor que el segundo");
+        }  else if ( numeroUno == numeroDos) {
+            System.out.println("Ambos numeros son iguales");
+        }  else  {
+            System.out.println(" Numero uno mayor que numero dos");
+        }
+         
+        
         // 3. Dado un número, verifica si es positivo, negativo o cero.
-
+         int numeroA = -3;
+         if (numeroA < 0) {
+            System.out.println(" es negativo");
+        }  else if ( numeroA == 0 ) {
+            System.out.println("Es cero");
+        }  else  {
+            System.out.println(" es positivo");
+        }
         // 4. Crea un programa que diga si un número es par o impar.
 
         // 5. Verifica si un número está en el rango de 1 a 100.
-
+        int numeroX = 110;
+        if (numeroX >= 1 && numeroX <= 99) {
+            System.out.println("el numero esta en el intervalo");   
+        } else {
+            System.out.println("el numero no esta en el intervalo");
+        }
         // 6. Declara una variable con el día de la semana (1-7) y muestra su nombre con switch.
+        int diaSemana = 5;
+        switch(diaSemana) {
+            case 1:
+        System.out.println("Lunes");
+        break;
+            case 2:
+        System.out.println("Martes");
+        break;
+            case 3:
+        System.out.println("miercoles");
+        break;
+           case 4:
+        System.out.println("jueves");
+        break;
+           case 5:
+        System.out.println("viernes");
+        break;
+        default :
+        System.out.println("fin de semana");
+            }
 
+        }
         // 7. Simula un sistema de notas: muestra "Sobresaliente", "Aprobado" o "Suspenso" según la nota (0-100).
 
         // 8. Escribe un programa que determine si puedes entrar al cine: debes tener al menos 15 años o ir acompañado.
@@ -30,4 +78,4 @@ public class ConditionalsExercises {
         // 10. Usa tres variables a, b, c y muestra cuál es el mayor de las tres.
 
     }
-}
+
